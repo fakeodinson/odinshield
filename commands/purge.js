be2b1f59a9ea353c973f0c032523262f6356c85c
Odinson <nurrahman.nrs@gmail.com>
2020-09-01 13:14:00 +0700

    new command:userinfo.js

--- commands/purge.js
+++ commands/purge.js
@@ -11,13 +11,23 @@ module.exports = {
         if(!message.member.hasPermission("MANAGE_MESSAGES"))
         return;
 
-        const deleteCount = parseInt(args[1], 10);
+        const deleteCount = parseInt(args[1] -(-1), 10);
 
         if(!deleteCount || deleteCount < 2 || deleteCount > 1000)
-          return message.reply("provide a number between 2 and 1000 for the number of messages to be deleted.");
+          return message.reply("provide a number between 1 and 1000 for the number of messages to be deleted.");
 
         const fetched = await message.channel.fetchMessages({limit: deleteCount});
         message.channel.bulkDelete(fetched)
           .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+        
+          const timeout = 5000
+          const embed = new Discord.RichEmbed()
+          .setColor('fc9403')
+          .setTitle(`:pencil: deleted ${deleteCount - 1} message(s)`)
+          .setDescription(`this message will be auto deleted in ${timeout - 4995}s`)
+
+        const waitdelete = await message.channel.send(embed);
+        waitdelete.delete(timeout);
+
       }
   }
